cb254a84d696decf868fd8ba12ba7bdd128481c0
Paweł krawczyk <pawel.krawczyk@hush.com>
2011-03-02 23:28:44 +0100

    rewritten responses map function

--- survey/views/q-and-resp/map.js
+++ survey/views/q-and-resp/map.js
@@ -2,13 +2,32 @@ function(doc) {
 	// Output list of surveys keyed by their id and responses keyed by the survey they
 	// respond to. Surveys are marked by 0, responses by 1. key= parameter can be used
 	// to limit view to only one reponse
-	if(doc.type == "survey") {
-		emit([doc._id, 0], doc);
+	if(doc.type == "survey" && doc.title && doc.order && doc.order.length > 0) {
+		for(var i=0; i<doc.order.length; i++) {
+			var q_name = doc.order[i]; //question1
+			var q = doc[q_name]; // table doc.question1
+			var a = q['answers'];
+			for(var j=0; j<a.length; j++) {
+				emit([doc._id, q_name, 0, q.ask, a[j]], 0);
+			}
+		}
 	} else
-	if(doc.type == "survey_response") {
-		emit([doc.response_to, 1], doc);
+	if(doc.type == "survey_response" && doc.response_to) {
+		for(var key in doc) {
+			// iterate through key to determine answers
+			if(key != "_id" && key != "_rev" && key != "created_at"
+				&& key != "response_to" && key != "type") {
+				// this must be answer
+				var q = key;
+				var a = doc[q];
+				if(typeof(a) == "string")
+					emit([doc.response_to, q, 1, a], 1);
+				if(typeof(a) == "object") {
+					for(var k=0; k<doc[key].length; k++) {
+						emit([doc.response_to, q, 1, a[k]], 1);
+					}
+				}
+			}
+		}
 	}
 }
-// Sample output:
-//{"id":"5baf93a6d0e7fe88ba1540506500031c","key":["5baf93a6d0e7fe88ba1540506500031c",0],"value":{"_id":"5baf93a6d0e7fe88ba1540506500031c","_rev":"4-a6a4b5702fabf72947e78054af06a2b2","type":"survey","title":"Animals survey","order":["question1","question2","question3"],"question1":{"ask":"What animal do you like most?","type":"radio","answers":["Cat","Horse","Donkey","Dog"]},"question2":{"ask":"What colors do you like?","type":"checkbox","answers":["Red","Brown","Black","Green"]},"question3":{"ask":"Please enter your name","type":"text","answers":[""]}}},
-//{"id":"55de14aadccf3cfa8dd8af4154002868","key":["5baf93a6d0e7fe88ba1540506500031c",1],"value":{"_id":"55de14aadccf3cfa8dd8af4154002868","_rev":"1-b4e359812f759f6d4cdf116eaff0bd4c","question1":"Cat","question2":"Red","question3":"Pawe\u0142","type":"survey_response","response_to":"5baf93a6d0e7fe88ba1540506500031c","created_at":"2011-02-24T20:49:29.853Z"}}
