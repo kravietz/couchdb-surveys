a65e043d8a2f35d29cd4531ea5dcf59eb4eb8272
Paweł krawczyk <pawel.krawczyk@hush.com>
2011-03-02 23:25:54 +0100

    new responses list function

--- survey/lists/responses.js
+++ survey/lists/responses.js
@@ -11,20 +11,36 @@ function(head, req) {
 	var survey_list = Path.list('surveys', 'all-surveys');
 	var survey_results = Path.list('results', 'all-results');
     var data = {
-      page_title : "Available responses",
+      page_title : "Responses to survey X", // XXX
       site_title : this.couchapp.name,
       abs : abs,
       survey_list : survey_list,
       survey_results : survey_results,
-      surveys : []
+      questions : []
     };
     provides("html", function() {
-    while (row = getRow()) {
-      data.surveys.push({
-        title : row.value.title,
-        id : row.value._id
-      }); //push
-    }; //while row
-    return Mustache.to_html(ddoc.templates.responses, data, ddoc.templates.partials);
+    	var last_q;
+    	var question = {q : "", a : []};
+	    while (row = getRow()) {
+	    	var type = row.key[2]; //0=question,1=answers
+	    	var id = row.key[0];
+	    	var q_id = row.key[1]; //question1
+	    	var text = row.key[3];
+	    	var count = row.value;
+	    	log([text, type, question]);
+	    	if(type == 0 && question.a.length > 0) {
+	    		data.questions.push(JSON.parse(JSON.stringify(question))); // push clone
+	    		question.a = [];
+	    	}
+	    	if(type == 0) {
+	    		question.q = text;
+	    	}
+	    	if (type == 1) {
+	    		question.a.push({ answer : text, count : count });
+	    	}    
+	    }; //while row
+	    data.questions.push(JSON.parse(JSON.stringify(question))); // push last chunk
+	   // return JSON.stringify(data);
+	    return Mustache.to_html(ddoc.templates.responses, data, ddoc.templates.partials);
   }); //provides
 }
